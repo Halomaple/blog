3578b847fe3992bf91dce6348565eef668117755
Jianfeng <jeff.doyle@foxmail.com>
2017-12-28 23:34:14 +0800

    Fix progress bar's width

--- assets/js/progressBar.js
+++ assets/js/progressBar.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+	if ($('.post-content').length === 0) return;
+
 	var progressBarHtml = '<div id="reading-progress-bar" style="z-index: 100000; position: fixed; top: 0; left: 0;  height: 4px; background-color: #35a6c9;"></div>';
 	$('body').append(progressBarHtml);
 
-	var documentObj = $(document);
+	var documentObj = $(document),
+		windowObj = $(window);
 	documentObj.scroll(function() {
-		$('#reading-progress-bar').width(documentObj.scrollTop() / documentObj.height() * 100 + 2.3 + '%');
+		$('#reading-progress-bar').width((documentObj.scrollTop() + windowObj.height()) / documentObj.height() * 100 + '%');
 	});
 });
